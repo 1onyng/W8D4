69d9582cc1333513028d4dea6bf907c5762a38e6
Tony Ng <tonyng987@gmail.com>
2019-08-22 16:34:48 -0700

    Complete searchable for ActiveRecord project

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,20 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    
+    where_line = params.keys.map { |key| "#{key} = ?" }.join(" AND ")
+    array = DBConnection.execute(<<-SQL, *params.values)
+      SELECT 
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{where_line}  
+    SQL
+
+    parse_all(array)
   end
 end
 
 class SQLObject
-  
+  extend Searchable 
 end
